ebe67da176bf4cf7e9fde01c32af36baed4aed37
Ondrej Kubala <kub.ond@gmail.com>
2017-05-08 08:31:06 -0300

    Creates models for our app and migrations.

--- app.rb
+++ app.rb
@@ -4,6 +4,9 @@ require 'sinatra/activerecord'
 
 require './config/database'
 
+# Load Models
+Dir["./app/models/*.rb"].each {|file| require file }
+
 class App < Sinatra::Base
   get '/' do
     'Hello world!'
---
+++ db/migrate/20170508110735_create_hashtags.rb
@@ -0,0 +1,8 @@
+class CreateHashtags < ActiveRecord::Migration[5.1]
+  def change
+    create_table :hashtags do |t|
+      t.string :name
+      t.integer :company_id
+    end
+  end
+end
